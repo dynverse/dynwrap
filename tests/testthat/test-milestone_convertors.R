5bd0b14959ee2ddce9c925716c250222d1f80677
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-12 16:21:03 +0200

    clean test

--- tests/testthat/test-milestone_convertors.R
+++ tests/testthat/test-milestone_convertors.R
@@ -1,64 +1,61 @@
-context("Testing milestone_convertors.R")
+context("Testing percentage convertors")
 
-test_that("Testing milestone_convertors.R", {
-  cell_ids <- letters[1:10]
-  milestone_ids <- LETTERS[1:4]
-  milestone_network <- tribble(
-    ~from, ~to, ~length, ~directed,
-    "A",   "B", 1,       TRUE,
-    "B",   "C", 2,       TRUE,
-    "B",   "D", 3,       TRUE
-  )
-  milestone_percentages <- tribble(
-    ~cell_id, ~milestone_id, ~percentage,
-    "a",      "A",           1,
+cell_ids <- letters[1:10]
+milestone_ids <- LETTERS[1:4]
+milestone_network <- tribble(
+  ~from, ~to, ~length, ~directed,
+  "A",   "B", 1,       TRUE,
+  "B",   "C", 2,       TRUE,
+  "B",   "D", 3,       TRUE
+)
+milestone_percentages <- tribble(
+  ~cell_id, ~milestone_id, ~percentage,
+  "a",      "A",           1,
 
-    "b",      "A",           .25,
-    "b",      "B",           .75,
+  "b",      "A",           .25,
+  "b",      "B",           .75,
 
-    "c",      "A",           0,
-    "c",      "B",           1,
+  "c",      "A",           0,
+  "c",      "B",           1,
 
-    "d",      "B",           .3,
-    "d",      "C",           .7,
+  "d",      "B",           .3,
+  "d",      "C",           .7,
 
-    "e",      "B",           .7,
-    "e",      "D",           .3,
+  "e",      "B",           .7,
+  "e",      "D",           .3,
 
-    "f",      "B",           1,
+  "f",      "B",           1,
 
-    "g",      "B",           .3,
-    "g",      "C",           .4,
-    "g",      "D",           .3,
+  "g",      "B",           .3,
+  "g",      "C",           .4,
+  "g",      "D",           .3,
 
-    "h",      "C",           1,
+  "h",      "C",           1,
 
-    "i",      "B",           1,
+  "i",      "B",           1,
 
-    "j",      "B",           0,
-    "j",      "C",           .5,
-    "j",      "D",           .5
-  )
-  progressions <- tribble(
-    ~cell_id, ~from, ~to, ~percentage,
-    "a",      "A",   "B", 0,
-    "b",      "A",   "B", .75,
-    "c",      "A",   "B", 1,
-    "d",      "B",   "C", .7,
-    "e",      "B",   "D", .3,
-    "f",      "A",   "B", 1,
-    "g",      "B",   "C", .4,
-    "g",      "B",   "D", .3,
-    "h",      "B",   "C", 1,
-    "i",      "A",   "B", 1,
-    "j",      "B",   "C", .5,
-    "j",      "B",   "D", .5
-  )
+  "j",      "B",           0,
+  "j",      "C",           .5,
+  "j",      "D",           .5
+)
+progressions <- tribble(
+  ~cell_id, ~from, ~to, ~percentage,
+  "a",      "A",   "B", 0,
+  "b",      "A",   "B", .75,
+  "c",      "A",   "B", 1,
+  "d",      "B",   "C", .7,
+  "e",      "B",   "D", .3,
+  "f",      "A",   "B", 1,
+  "g",      "B",   "C", .4,
+  "g",      "B",   "D", .3,
+  "h",      "B",   "C", 1,
+  "i",      "A",   "B", 1,
+  "j",      "B",   "C", .5,
+  "j",      "B",   "D", .5
+)
 
+test_that("Testing convert_milestone_percentages_to_progressions", {
   progressions_calc <- convert_milestone_percentages_to_progressions(cell_ids, milestone_ids, milestone_network, milestone_percentages)
-
-  milestone_percentages_calc <- convert_progressions_to_milestone_percentages(cell_ids, milestone_ids, milestone_network, progressions)
-
   prog_control <- progressions %>% rename(orig = percentage) %>%
     full_join(progressions_calc %>% rename(calc = percentage), by = c("cell_id", "from", "to")) %>%
     mutate(
@@ -66,7 +63,10 @@ test_that("Testing milestone_convertors.R", {
       check = !is.na(orig) & !is.na(calc) & diff < 1e-8
     )
   expect_true(all(prog_control$check))
+})
 
+test_that("Testing convert_progressions_to_milestone_percentages", {
+  milestone_percentages_calc <- convert_progressions_to_milestone_percentages(cell_ids, milestone_ids, milestone_network, progressions)
   perc_control <- milestone_percentages %>% rename(orig = percentage) %>%
     full_join(milestone_percentages_calc %>% rename(calc = percentage), by = c("cell_id", "milestone_id")) %>%
     mutate(
