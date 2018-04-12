8374ef2449cafdaeeb0a05b04f9e5979e327ff0f
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-12 16:32:16 +0200

    test whether an edge in a progressions is included in the milestone network

--- R/convert_progressions_to_milestone_percentages.R
+++ R/convert_progressions_to_milestone_percentages.R
@@ -5,10 +5,20 @@
 #' @param progressions Progressions dataframe
 #' @export
 convert_progressions_to_milestone_percentages <- function(cell_ids, milestone_ids, milestone_network, progressions) {
-  check_froms <- progressions %>% group_by(cell_id) %>% summarise(n = length(unique(from)))
+  check_froms <- progressions %>%
+    group_by(cell_id) %>%
+    summarise(n = length(unique(from)))
   if (any(check_froms$n > 1)) {
     stop("In ", sQuote("progressions"), ", cells should only have 1 unique from milestone.")
   }
+
+  check_edges <- progressions %>%
+    left_join(milestone_network, by = c("from", "to")) %>%
+    left_join(milestone_network %>% select(to = from, from = to, length2 = length), by = c("from", "to"))
+  if (any(is.na(check_edges$length) & is.na(check_edges$length2))) {
+    stop("All from-to combinations in ", sQuote("progressions"), " should be in ", sQuote("milestone_network"), " as well.")
+  }
+
 
   froms <- progressions %>%
     group_by(cell_id) %>%
