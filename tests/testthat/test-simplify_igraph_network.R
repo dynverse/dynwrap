e36e4075a92b4ebc30f800f84e7580ffc5aeab5d
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-13 13:52:49 +0200

    expand test

--- tests/testthat/test-simplify_igraph_network.R
+++ tests/testthat/test-simplify_igraph_network.R
@@ -2,6 +2,86 @@ context("Testing simplify_igraph_network")
 
 tests <- list(
   list(
+    name = "directed_linear",
+    directed = TRUE,
+    net = tribble(
+      ~from, ~to,
+      1, 2,
+      2, 3
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 3, 2, TRUE
+    )
+  ),
+  list(
+    name = "directed_diverging",
+    directed = TRUE,
+    net = tribble(
+      ~from, ~to,
+      1, 2,
+      1, 3
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 2, 1, TRUE,
+      1, 3, 1, TRUE
+    )
+  ),
+  list(
+    name = "undirected_linear",
+    directed = FALSE,
+    net = tribble(
+      ~from, ~to,
+      1, 2,
+      1, 3
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      2, 3, 2, FALSE
+    )
+  ),
+  list(
+    name = "simple_branching",
+    directed = TRUE,
+    net = tribble(
+      ~from, ~to,
+      1, 2,
+      2, 3,
+      2, 4,
+      3, 5,
+      3, 6
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 2, 1, TRUE,
+      2, 3, 1, TRUE,
+      2, 4, 1, TRUE,
+      3, 5, 1, TRUE,
+      3, 6, 1, TRUE
+    )
+  ),
+  list(
+    name = "simple_undirected_branching",
+    directed = FALSE,
+    net = tribble(
+      ~from, ~to,
+      1, 2,
+      2, 3,
+      2, 4,
+      3, 5,
+      3, 6
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 2, 1, FALSE,
+      2, 3, 1, FALSE,
+      2, 4, 1, FALSE,
+      3, 5, 1, FALSE,
+      3, 6, 1, FALSE
+    )
+  ),
+  list(
     name = "branching",
     directed = TRUE,
     net = tribble(
@@ -13,30 +93,30 @@ tests <- list(
       5, 6, 5
     ),
     expected_net = tribble(
-      ~from, ~to, ~weight,
-      1, 3, 3,
-      3, 4, 3,
-      3, 6, 9
+      ~from, ~to, ~weight, ~directed,
+      1, 3, 3, TRUE,
+      3, 4, 3, TRUE,
+      3, 6, 9, TRUE
     )
   ),
-  # list(
-  #   name = "branching_converging",
-  #   directed = TRUE,
-  #   net = tribble(
-  #     ~from, ~to, ~weight,
-  #     1, 2, 1,
-  #     2, 3, 2,
-  #     3, 4, 3,
-  #     4, 5, 4,
-  #     3, 5, 5
-  #   ),
-  #   expected_net = tribble(
-  #     ~from, ~to, ~weight,
-  #     1, 3, 3,
-  #     3, 5, 7,
-  #     3, 5, 5
-  #   )
-  # ),
+  list(
+    name = "branching_converging",
+    directed = TRUE,
+    net = tribble(
+      ~from, ~to, ~weight,
+      1, 2, 1,
+      2, 3, 2,
+      3, 4, 3,
+      4, 5, 4,
+      3, 5, 5
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 3, 3, TRUE,
+      3, 5, 7, TRUE,
+      3, 5, 5, TRUE
+    )
+  ),
   list(
     name = "cyclical",
     directed = TRUE,
@@ -47,26 +127,26 @@ tests <- list(
       3, 1, 3
     ),
     expected_net = tribble(
-      ~from, ~to, ~weight,
-      1, 1, 6
+      ~from, ~to, ~weight, ~directed,
+      1, 1, 6, TRUE
     )
-  # ),
-  # list(
-  #   name = "undirected_branching_converging",
-  #   directed = FALSE,
-  #   net = tribble(
-  #     ~from, ~to, ~weight,
-  #     1, 2, 1,
-  #     3, 2, 2,
-  #     3, 4, 3,
-  #     5, 4, 4,
-  #     3, 5, 5
-  #   ),
-  #   expected_net = tribble(
-  #     ~from, ~to, ~weight,
-  #     1, 3, 3,
-  #     3, 3, 12
-  #   )
+  ),
+  list(
+    name = "undirected_branching_converging",
+    directed = FALSE,
+    net = tribble(
+      ~from, ~to, ~weight,
+      1, 2, 1,
+      3, 2, 2,
+      3, 4, 3,
+      5, 4, 4,
+      3, 5, 5
+    ),
+    expected_net = tribble(
+      ~from, ~to, ~weight, ~directed,
+      1, 3, 3, FALSE,
+      3, 3, 12, FALSE
+    )
   )
 )
 
@@ -84,16 +164,14 @@ for (test in tests) {
 
     pass_check <- all(!is.na(control$left) & !is.na(control$right))
 
-    # if (!pass_check) {
-    #   cat("Failed ", test$name, " ", ifelse(test$directed, "(directed)", "(undirected)"), "\n", sep = "")
-    #   cat("Original:\n")
-    #   print(test$net)
-    #   cat("Expected:\n")
-    #   print(test$expected_net)
-    #   cat("Got:\n")
-    #   print(newnet)
-    #   cat("============================\n")
-    # }
+    # cat(ifelse(pass_check, "SUCCEEDED!", "FAILED!"), " ", test$name, " ", ifelse(test$directed, "(directed)", "(undirected)"), "\n", sep = "")
+    # cat("Original:\n")
+    # print(test$net %>% as.data.frame)
+    # cat("Expected:\n")
+    # print(test$expected_net %>% as.data.frame)
+    # cat("Got:\n")
+    # print(newnet)
+    # cat("============================\n")
     expect_true( pass_check )
     expect_true( all(c("from", "to", "weight") %in% colnames(newnet)) )
   })
