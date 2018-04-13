f09679dc10a5b9e11cd8336b09eab376ae46cd0b
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-13 17:51:47 +0200

    explicitly test names for cyclic as well; add directedness parameter; don't error when not all pseudotimes are given

--- R/wrap_add_cyclic_trajectory_to_wrapper.R
+++ R/wrap_add_cyclic_trajectory_to_wrapper.R
@@ -4,16 +4,18 @@
 #'
 #' @param data_wrapper A data wrapper to extend upon.
 #' @param pseudotimes A named vector of pseudo times.
+#' @param directed Whether or not the directionality of the pseudotime is predicted.
 #' @param do_scale_minmax Whether or not to scale the pseudotimes between [0,1].
 #'   Otherwise, will assume the values are already within that range.
 #' @param ... extra information to be stored in the wrapper.
 #'
 #' @export
 #'
-#' @importFrom testthat expect_is expect_true
+#' @importFrom testthat expect_is expect_true expect_named
 add_cyclic_trajectory_to_wrapper <- function(
   data_wrapper,
   pseudotimes,
+  directed = FALSE,
   do_scale_minmax = TRUE,
   ...
 ) {
@@ -23,6 +25,7 @@ add_cyclic_trajectory_to_wrapper <- function(
   # check names of pseudotimes
   cell_ids <- data_wrapper$cell_ids
   testthat::expect_is(pseudotimes, "numeric")
+  testthat::expect_named(pseudotimes)
   testthat::expect_true(all(names(pseudotimes) %in% cell_ids))
 
   # scale pseudotimes
@@ -39,7 +42,7 @@ add_cyclic_trajectory_to_wrapper <- function(
   milestone_network <- tibble(
     from = milestone_ids,
     to = milestone_ids[c(2,3,1)],
-    directed = TRUE,
+    directed = directed,
     length = 1,
     edge_id = seq_along(milestone_ids)
   )
@@ -47,7 +50,7 @@ add_cyclic_trajectory_to_wrapper <- function(
   # construct progressions
   progressions <- tibble(
     time = 3 * pseudotimes,
-    cell_id = cell_ids
+    cell_id = names(pseudotimes)
   ) %>%
     mutate(edge_id = ifelse(time <= 1, 1L, ifelse(time <= 2, 2L, 3L))) %>%
     left_join(milestone_network, by = "edge_id") %>%
