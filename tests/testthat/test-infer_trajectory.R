eb22eae5a5cef8584eab20c81013a03b749ce9c9
zouter <wouter.saelens@gmail.com>
2018-05-31 11:58:29 +0200

    fix infer trajectory test

--- tests/testthat/test-infer_trajectory.R
+++ tests/testthat/test-infer_trajectory.R
@@ -2,10 +2,39 @@ context("Testing infer_trajectory")
 
 
 test_that("Testing infer_trajectory with control methods", {
-  data("toy_tasks", package="dyntoy")
+  # create task
+  id <- "a"
+  cell_ids <- c("truth", "universally", "acknowledged", "that", "a", "single")
+  cell_info <- data_frame(
+    cell_id = cell_ids,
+    info1 = c("man", "in", "possession", "of", "a", "good"),
+    info2 = c("fortune", "must", "be", "in", "want", "of"),
+    info3 = 1:6
+  )
+  extras1 <- list("a wife.")
+  extras2 <- c("However", "little", "known")
 
-  # run with one task and one method
-  task <- toy_tasks %>% extract_row_to_list(1)
+  num_features <- round(runif(1, 100, 120))
+  feature_names <- paste0("feature_", seq_len(num_features))
+
+  expression <- matrix(runif(num_features * length(cell_ids), 8, 12), nrow = length(cell_ids), dimnames = list(cell_ids, feature_names))
+  counts <- 2^expression - 1
+  feature_info <- data_frame(feature_id = feature_names, mean = colMeans(expression), var = apply(expression, 2, var))
+
+  task <-
+    wrap_expression(
+      id = id,
+      expression,
+      counts,
+      cell_info,
+      feature_info,
+      extras1=extras1,
+      extras2=extras2
+    ) %>%
+    add_prior_information(start_cells = cell_ids[[1]])
+
+
+
   method <- ti_comp1()
 
   model <- infer_trajectory(task, method)
@@ -55,7 +84,7 @@ test_that("Testing infer_trajectory with control methods", {
 
   # run with multiple tasks and multiple methods with specified parameters
   models <- infer_trajectories(
-    task = toy_tasks[c(1,2),],
+    task = list(task, task),
     method = list_as_tibble(list(ti_comp1(), ti_comp1())),
     parameters = list(list(method = "mds"), list(method = "pca"))
   )
