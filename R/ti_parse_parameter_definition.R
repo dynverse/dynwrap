a2cf19de14ced4d66bc4291e8fa6dae5776f581c
zouter <wouter.saelens@gmail.com>
2018-06-04 15:01:27 +0200

    allow forbidden

--- R/ti_parse_parameter_definition.R
+++ R/ti_parse_parameter_definition.R
@@ -1,4 +1,10 @@
 parse_parameter_definition <- function(parameter_definition) {
+  if(!is.null(parameter_defintition$forbidden)) {
+    forbidden <- quote(parse(text = parameter_defintition$forbidden))
+  } else {
+    forbidden <- NULL
+  }
+
   map2(names(parameter_definition), parameter_definition, function(id, p) {
     if(p$type == "integer") {
       ParamHelpers::makeIntegerParam(id, lower=p$lower, upper=p$upper, default=p$default)
@@ -8,5 +14,5 @@ parse_parameter_definition <- function(parameter_definition) {
       stop("invalid type")
     }
   }) %>%
-    do.call(ParamHelpers::makeParamSet, .)
+    invoke(ParamHelpers::makeParamSet, ., forbidden = forbidden)
 }
