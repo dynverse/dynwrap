5a8637602f28e534ef25f3835f98a8e8b9568a12
zouter <wouter.saelens@gmail.com>
2018-06-20 21:02:57 +0200

    cleanup docker creation, add quick and expanded testing for docker

---
+++ R/container_helpers.R
@@ -0,0 +1,39 @@
+#' Tests whether docker is correctly installed and available
+#'
+#' @param detailed Whether top do a detailed check
+#' @export
+test_docker_installation <- function(detailed = FALSE) {
+  if (!detailed) {
+    version <- suppressWarnings(system("docker version", intern = TRUE))
+    if (!is.null(attr(version, "status")) && attr(version, "status") == 1) {
+      FALSE
+    } else {
+      TRUE
+    }
+  } else {
+    # test if docker command is found
+    tryCatch({
+      version <- system("docker", intern = TRUE, ignore.stderr = TRUE)
+      message(crayon::green("\u2714 Docker is installed"))
+    },
+    error = function(e) {
+      stop(crayon::red("\u274C An installation of docker is necessary to run this method. See https://docs.docker.com/install/ for instructions."))
+    })
+
+    # test if docker daemon is running
+    version <- suppressWarnings(system("docker version --format '{{.Client.APIVersion}}'", intern = TRUE))
+    if (!is.null(attr(version, "status")) && attr(version, "status") == 1) {
+      stop(crayon::red(glue::glue("\u274C Docker daemon does not seem to be running... \n- Try running {crayon::bold('dockerd')} in the command line \n- See https://docs.docker.com/config/daemon/")))
+    }
+    message(crayon::green("\u2714 Docker daemon is running"))
+
+    # test if docker version is recent enough
+    if (utils::compareVersion("1.0", version) > 0) {
+      stop(crayon::red("\u274C Docker API version is", version, ". Requires 1.0 or later"))
+    }
+
+    message(crayon::green("\u2714 Docker is at correct version"))
+
+    TRUE
+  }
+}
