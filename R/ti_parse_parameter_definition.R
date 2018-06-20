1246495729a1e208e17e8195fb133f16e5e64e2d
zouter <wouter.saelens@gmail.com>
2018-06-20 10:59:48 +0200

    make all params tunable by default

--- R/ti_parse_parameter_definition.R
+++ R/ti_parse_parameter_definition.R
@@ -16,12 +16,12 @@ parse_parameter_definition <- function(parameter_definition) {
     }
 
     if (is.null(param$special_values)) param$special_values <- list()
+    if (is.null(param$tunable)) param$tunable <- TRUE
 
     if(param$type %in% c("integer", "numeric", "integer_vector", "numeric_vector")) {
       if (is.null(param$lower)) param$lower <- param$default
       if (is.null(param$upper)) param$upper <- param$default
       if (is.null(param$distribution)) param$distribution <- "uniform"
-      if (is.null(param$tunable)) param$tunable <- TRUE
       distribution2uniform <- get_distribution2uniform(param)
       uniform2distribution <- get_uniform2distribution(param)
 
