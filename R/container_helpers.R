7932488f08b7fcb5a52985d1fc5fecac4960ac55
zouter <wouter.saelens@gmail.com>
2018-06-26 10:01:19 +0200

    fix test_docker_installation

--- R/container_helpers.R
+++ R/container_helpers.R
@@ -4,12 +4,15 @@
 #' @export
 test_docker_installation <- function(detailed = FALSE) {
   if (!detailed) {
-    version <- suppressWarnings(system("docker version", intern = TRUE, ignore.stdout = TRUE, ignore.stderr = TRUE))
-    if (is.null(attr(version, "status")) || attr(version, "status") == "0") {
-      TRUE
-    } else {
-      FALSE
-    }
+    tryCatch(
+      {
+        version <- suppressWarnings(system("docker version", intern = TRUE, ignore.stdout = TRUE, ignore.stderr = TRUE))
+        if (!is.null(attr(version, "status")) && attr(version, "status") == "0") stop()
+
+        TRUE
+      },
+      error = function(e) FALSE
+    )
   } else {
     # test if docker command is found
     tryCatch({
