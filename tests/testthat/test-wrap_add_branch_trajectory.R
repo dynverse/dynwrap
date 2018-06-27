4457a0f092ce830a3bce0a5a0315abd023a1c3c5
zouter <wouter.saelens@gmail.com>
2018-06-27 16:39:07 +0200

    expand branch trajectory testing

--- tests/testthat/test-wrap_add_branch_trajectory.R
+++ tests/testthat/test-wrap_add_branch_trajectory.R
@@ -1,8 +1,8 @@
 context("Testing add_trajectory")
 
 id <- "a"
-cell_ids <- letters[1:10]
-branch_ids <- c("A", "B", "C", "D")
+cell_ids <- letters[1:12]
+branch_ids <- c("A", "B", "C", "D", "E")
 
 branch_network <- tribble(
   ~from, ~to,
@@ -12,12 +12,12 @@ branch_network <- tribble(
 )
 branch_progressions <- tibble(
   cell_id = cell_ids,
-  branch_id = c("A", "A", "A", "B", "B", "C", "C", "D", "D", "D"),
-  percentage = c(0, 0.5, 1, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1)
+  branch_id = c("A", "A", "A", "B", "B", "C", "C", "D", "D", "D", "E", "E"),
+  percentage = c(0, 0.5, 1, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 1, 0.1, 0.6)
 )
 branches <- tibble(
   branch_id = branch_ids,
-  length = as.numeric(1:4),
+  length = as.numeric(1:5),
   directed = TRUE
 )
 
@@ -41,6 +41,6 @@ test_that("Testing add_trajectory with milestone_percentages", {
 
   expect_equivalent(wr$id, id)
   expect_equivalent(wr$cell_ids, cell_ids)
-  expect_equivalent(wr$milestone_network$from, c("1", "2", "3", "3"))
-  expect_equivalent(wr$milestone_network$to, c("2", "3", "4", "5"))
+  expect_equivalent(wr$milestone_network$from, c("1", "2", "3", "3", "4"))
+  expect_equivalent(wr$milestone_network$to, c("2", "3", "5", "6", "7"))
 })
