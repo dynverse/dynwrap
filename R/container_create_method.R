0c7604341c91e493a5dd22fa3e570e0023bf46ac
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-26 09:55:16 +0200

    add create_container_ti_method

--- R/container_create_method.R
+++ R/container_create_method.R
@@ -286,6 +286,32 @@ extract_definition_from_docker_image <- function(
   definition
 }
 
+#' Creating a TI method from a docker repository
+#'
+#' Supports both docker and singularity as a backend.
+#'
+#' @param docker_repository The id of the docker repository.
+#' @param run_environment In which environment to run the method, can be `"docker"` or `"singularity"`.
+#' @param ... Default parameters for the method.
+create_container_ti_method <- function(docker_repository, run_environment = NULL, ...) {
+  if (is.null(run_environment)) {
+    run_environment <- getOption("dynwrap_run_environment")
+  }
+  if (is.null(run_environment)) {
+    run_environment <- "docker"
+  }
+
+  # choose environments
+  if (run_environment == "docker") {
+    method <- create_docker_ti_method(docker_repository)
+  } else if (run_environment == 'singularity') {
+    method <- create_singularity_ti_method(docker_repository, ".simg")
+  }
+
+  method(...)
+}
+
+
 #' @rdname create_docker_ti_method
 #' @export
 pull_docker_ti_method <- function(
