4676336d2f0b8263b517f4ccc3672c44f7c770a7
zouter <wouter.saelens@gmail.com>
2018-08-08 11:08:47 +0200

    add gather at milestones

---
+++ R/wrap_gather_cells_at_milestones.R
@@ -0,0 +1,23 @@
+#' "Gather" cells to their closest milestones
+#'
+#' @param trajectory The trajectory
+#' @export
+gather_cells_at_milestones <- function(trajectory) {
+  testthat::expect_true(is_wrapper_with_trajectory(trajectory))
+
+  milestone_percentages <- trajectory$milestone_percentages %>%
+    group_by(cell_id) %>%
+    arrange(-percentage) %>%
+    slice(1) %>%
+    mutate(percentage = 1) %>%
+    ungroup()
+
+  trajectory %>%
+    add_trajectory(
+      milestone_network = trajectory$milestone_network,
+      divergence_regions = trajectory$divergence_regions,
+      milestone_percentages = milestone_percentages
+    )
+}
+
+
