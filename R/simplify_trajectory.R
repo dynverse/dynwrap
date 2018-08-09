80eeff65773ab65dd4b18269291a6295bca78e1a
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-09 13:08:12 +0200

    add simplify trajectory function

---
+++ R/simplify_trajectory.R
@@ -0,0 +1,29 @@
+
+#' Simplify a trajectory
+#'
+#' @param traj A trajectory to simplify
+#'
+#' @export
+simplify_trajectory <- function(traj) {
+  out <- simplify_igraph_network(
+    gr = igraph::graph_from_data_frame(traj$milestone_network %>% rename(weight = length), directed = TRUE, traj$milestone_ids),
+    allow_duplicated_edges = FALSE,
+    force_keep = unique(traj$divergence_regions$milestone_id),
+    edge_points = traj$progressions %>% rename(id = cell_id)
+  )
+  milestone_ids <- igraph::V(out$gr)$name
+  milestone_network <- igraph::as_data_frame(out$gr) %>%
+    select(from, to, length = weight, directed)
+  progressions <- out$edge_points %>% select(cell_id = id, from, to, percentage)
+
+  newtraj <- traj %>%
+    add_trajectory(
+      milestone_ids = milestone_ids,
+      milestone_network = milestone_network,
+      progressions = progressions,
+      divergence_regions = traj$divergence_regions
+    )
+  # if newtraj contains grouping, dimred, ..., remove them as necessary
+
+  newtraj
+}
