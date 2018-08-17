413d321e60fa81158dd8fbf15dbcc967a58e4fa9
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-17 15:15:55 +0200

    add test for safe_tempdir

---
+++ tests/testthat/test-helper_safe_tempdir.R
@@ -0,0 +1,10 @@
+context("Testing safe_tempdir")
+
+test_that("safe_tempdir works correctly", {
+  dir1 <- safe_tempdir("mydir")
+  dir2 <- safe_tempdir("mydir")
+  on.exit(unlink(dir1, recursive = TRUE))
+  on.exit(unlink(dir2, recursive = TRUE))
+
+  expect_true(dir1 != dir2)
+})
