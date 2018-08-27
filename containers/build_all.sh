6b2bd5a58fc2882a6ebb3c085c6cce3cbb23b1f1
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-27 08:33:53 +0200

    add synonymous tags

--- containers/build_all.sh
+++ containers/build_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 docker build r -t dynverse/dynwrap:r
 docker build bioc -t dynverse/dynwrap:bioc
+docker build py3.6 -t dynverse/dynwrap:py3.6
 docker build py3.6 -t dynverse/dynwrap:py3
 docker build py2.7 -t dynverse/dynwrap:py2.7
+docker build py2.7 -t dynverse/dynwrap:py2
 
 docker push dynverse/dynwrap
