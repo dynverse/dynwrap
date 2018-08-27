80e09bd552556155759d5e96802115bdb133000f
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-27 09:20:16 +0200

    use tagging instead of rebuild

--- containers/build_all.sh
+++ containers/build_all.sh
@@ -2,8 +2,8 @@
 docker build r -t dynverse/dynwrap:r
 docker build bioc -t dynverse/dynwrap:bioc
 docker build py3.6 -t dynverse/dynwrap:py3.6
-docker build py3.6 -t dynverse/dynwrap:py3
+docker tag dynverse/dynwrap:py3.6 dynverse/dynwrap:py3
 docker build py2.7 -t dynverse/dynwrap:py2.7
-docker build py2.7 -t dynverse/dynwrap:py2
+docker tag dynverse/dynwrap:py2.7 dynverse/dynwrap:py2
 
 docker push dynverse/dynwrap
