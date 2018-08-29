9fb07a2b74dcb2bf0fad1c9b260890ae263c4fb3
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-29 13:22:31 +0200

    update dynwrap containers

--- containers/build_all.sh
+++ containers/build_all.sh
@@ -4,4 +4,9 @@ docker build bioc -t dynverse/dynwrap:bioc
 docker build py3.6 -t dynverse/dynwrap:py3.6
 docker build py2.7 -t dynverse/dynwrap:py2.7
 
-docker push dynverse/dynwrap
+sudo singularity build r.simg bioc/Singularity.r
+sudo singularity build bioc.simg bioc/Singularity.bioc
+sudo singularity build py3.6.simg bioc/Singularity.py3.6
+sudo singularity build py2.7.simg bioc/Singularity.py2.7
+
+# docker push dynverse/dynwrap
