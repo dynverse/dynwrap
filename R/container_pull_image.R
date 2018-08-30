94a645fea74d924d31c6d3dfa0cd16fe635ae063
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-30 18:53:33 +0200

    allow to use concurrent cache function for other folders as well

--- R/container_pull_image.R
+++ R/container_pull_image.R
@@ -15,11 +15,11 @@
     # create directory if not present yet
     dir.create(image_folder, recursive = TRUE, showWarnings = FALSE)
 
-    tempcache <- .container_singularity_create_concurrent_cache()
-    on.exit(.container_singularity_finalise_concurrent_cache(tempcache))
+    tempcache <- .container_singularity_create_concurrent_cache(cachedir = image_folder)
+    on.exit(.container_singularity_finalise_concurrent_cache(tempcache, cachedir = image_folder))
 
     env <- c(
-      "SINGULARITY_PULL_FOLDER" = image_folder,
+      "SINGULARITY_PULL_FOLDER" = tempcache,
       "SINGULARITY_CACHEDIR" = tempcache
     )
 
