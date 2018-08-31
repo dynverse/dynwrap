08388321036d0d923308b17c71aa543cb7bf97e0
zouter <wouter.saelens@gmail.com>
2018-08-31 16:18:26 +0200

    fix docker debug statement, --it flag cannot be at the end (after the container location)

--- R/container_run.R
+++ R/container_run.R
@@ -93,7 +93,7 @@
     command <-
       switch(
         config$type,
-        docker = paste0(c(container_cmd, args, "-it"), collapse = " "),
+        docker = paste0(c(container_cmd, args[1], "-it", args[2:length(args)]), collapse = " "),
         singularity = paste0(c(paste0(names(env2), "=", env2, collapse = " "), container_cmd, args), collapse = " ")
       )
     stop("Use this command for debugging: \n", crayon::bold(command), call. = FALSE)
