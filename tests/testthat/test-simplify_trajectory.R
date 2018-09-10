24d7cef3ad4a668e68a53a32adeb76650f143c31
zouter <wouter.saelens@gmail.com>
2018-09-10 16:53:06 +0200

    #76, add test with complex graph, fixed by 76c6b16021c284c9dcf5c44f92299b56516e1bc3

--- tests/testthat/test-simplify_trajectory.R
+++ tests/testthat/test-simplify_trajectory.R
@@ -118,3 +118,51 @@ test_that("Test whether simplify is able to correctly simplify an undirected cyc
   expect_true(all(simp$milestone_network$from == c("A", "B", "A")))
   expect_true(all(simp$milestone_network$to == c("B", "C", "C")))
 })
+
+
+
+
+
+test_that("Test whether simplify is able to correctly simplify a graph", {
+  id <- "I am a graph! "
+  cell_ids <- c("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12")
+
+  milestone_ids <- c("A", "B", "C", "D", "E", "F")
+  milestone_network <- data_frame(
+    from = c("A", "A", "C", "C", "E", "B"),
+    to =   c("B", "D", "B", "D", "A", "F"),
+    length = c(1, 2, 3, 4, 5, 6),
+    directed = FALSE
+  )
+  progressions <- tribble(
+    ~cell_id, ~from, ~to, ~percentage,
+    "1", "A", "B", 0.3,
+    "2", "A", "D", 1.0,
+    "3", "C", "B", 0.5,
+    "4", "C", "D", 0.9,
+    "5", "E", "A", 0.0,
+    "6", "B", "F", 0.4,
+    "7","A", "B", 0.3,
+    "8","A", "D", 1.0,
+    "9","C", "B", 0.5,
+    "10","C", "D", 0.9,
+    "11","E", "A", 0.0,
+    "12","B", "F", 0.4
+  )
+
+  traj <-
+    wrap_data(
+      id = id,
+      cell_ids = cell_ids
+    ) %>%
+    add_trajectory(
+      milestone_ids = milestone_ids,
+      milestone_network = milestone_network,
+      progressions = progressions
+    )
+  simp <- simplify_trajectory(traj)
+
+  expect_true(all(cell_ids %in% simp$cell_ids))
+  expect_true(all(simp$milestone_network$from == c("A", "B", "A", "B", "A")))
+  expect_true(all(simp$milestone_network$to == c("B", "C", "E", "F", "C")))
+})
