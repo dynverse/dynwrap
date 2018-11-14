d9dfe0ae1f10d854603a3843ab0b836c7acb292d
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-14 12:54:35 +0100

    add colour to the wrapper types tibble

--- data-raw/wrapper_types.R
+++ data-raw/wrapper_types.R
@@ -1,14 +1,14 @@
 library(tidyverse)
 
 wrapper_types <- tribble(
-  ~id, ~long_name, ~short_name,
-  "direct", "Direct", "Direct",
-  "linear", "Linear pseudotime", "Linear",
-  "cyclic", "Cyclical pseudotime", "Cyclic",
-  "end_state_prob", "End state probability", "Prob",
-  "cluster_assignment", "Cluster assignment", "Cluster",
-  "orth_proj", "Orthogonal projection", "Proj",
-  "cell_graph", "Cell graph", "Cell"
+  ~id,                  ~long_name,              ~short_name, ~colour,
+  "direct",             "Direct",                "Direct",    "#ff3c2f",
+  "linear",             "Linear pseudotime",     "Linear",    "#0076dc",
+  "cyclic",             "Cyclical pseudotime",   "Cyclic",    "#2ec5c5",
+  "end_state_prob",     "End state probability", "Prob",      "#edd100",
+  "cluster_assignment", "Cluster assignment",    "Cluster",   "#ffa500",
+  "orth_proj",          "Orthogonal projection", "Proj",      "#2ecc40",
+  "cell_graph",         "Cell graph",            "Cell",      "#a000dc"
 )
 
 usethis::use_data(wrapper_types)
