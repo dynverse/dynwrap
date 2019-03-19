9513fef21cd622e29e27d2333b4fb8dcb02965c3
Wouter Saelens <wouter.saelens@gmail.com>
2019-03-19 10:11:02 +0100

    export is_ti_method

--- R/method_process_definition.R
+++ R/method_process_definition.R
@@ -265,6 +265,8 @@ get_default_parameters <- function(definition) {
 
 #' @param object The object to be tested
 #' @rdname definition
+#'
+#' @export
 is_ti_method <- function(object) {
   ("dynwrap::ti_method" %in% class(object)) || ("dynmethod::ti_method" %in% class(object))
 }
