038a175804e7a629b0a394941a4347521494fffe
Robrecht Cannoodt <rcannood@gmail.com>
2020-02-10 12:00:30 +0100

    simplify dimred_milestones and dimred_segments as well, including test

--- R/simplify_trajectory.R
+++ R/simplify_trajectory.R
@@ -19,19 +19,30 @@ simplify_trajectory <- function(trajectory, allow_self_loops = FALSE) {
     vertices = trajectory$milestone_ids
   )
 
+  # cell simplification
+  edge_points <- trajectory$progressions %>% rename(id = cell_id) %>% mutate(id = paste0("SIMPLIFYCELL_", id))
+
+  if (!is.null(trajectory$dimred_segment_points) && !is.null(trajectory$dimred_segment_progressions)) {
+    edge_points <- bind_rows(
+      edge_points,
+      trajectory$dimred_segment_progressions %>% mutate(id = paste0("SIMPLIFYSEGMENT_", seq_len(n())))
+    )
+  }
+
   out <- simplify_igraph_network(
     gr,
     allow_duplicated_edges = FALSE,
     allow_self_loops = allow_self_loops,
     force_keep = unique(trajectory$divergence_regions$milestone_id),
-    edge_points = trajectory$progressions %>% rename(id = cell_id)
+    edge_points = edge_points
   )
   milestone_ids <- igraph::V(out$gr)$name
   milestone_network <- igraph::as_data_frame(out$gr) %>%
     select(from, to, length = weight, directed) %>%
     distinct(from, to, .keep_all = TRUE)
   progressions <- out$edge_points %>%
-    select(cell_id = id, from, to, percentage)
+    filter(grepl("^SIMPLIFYCELL_", id)) %>%
+    transmute(cell_id = gsub("^SIMPLIFYCELL_", "", id), from, to, percentage)
 
   # test whether milestone_network and progressions contain the exact same edges
   different_edges <- progressions %>% group_by(from, to) %>% summarise() %>% anti_join(milestone_network, c("from", "to"))
@@ -47,6 +58,27 @@ simplify_trajectory <- function(trajectory, allow_self_loops = FALSE) {
       divergence_regions = trajectory$divergence_regions,
       allow_self_loops = allow_self_loops
     )
+
+  # cell simplification
+  if (!is.null(trajectory$dimred_milestones)) {
+    newtrajectory$dimred_milestones <- trajectory$dimred_milestones[newtrajectory$milestone_ids, , drop = FALSE]
+  }
+  if (!is.null(trajectory$dimred_segment_points) && !is.null(trajectory$dimred_segment_progressions)) {
+    filtered_segs <-
+      out$edge_points %>%
+      filter(grepl("^SIMPLIFYSEGMENT_", id)) %>%
+      mutate(id = as.integer(gsub("^SIMPLIFYSEGMENT_", "", id))) %>%
+      group_by(from, to) %>%
+      arrange(percentage) %>%
+      filter(!duplicated(percentage)) %>%
+      ungroup() %>%
+      arrange(from, to, percentage)
+    newtrajectory$dimred_segment_points <-
+      trajectory$dimred_segment_points[filtered_segs$id, , drop = FALSE]
+    newtrajectory$dimred_segment_progressions <-
+      filtered_segs %>% select(from, to, percentage)
+  }
+
   # TODO: if newtrajectory contains grouping, dimred, ..., remove them as necessary
   # We need a "filter_milestones" thing for this :)
 
